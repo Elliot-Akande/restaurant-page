363d2a80387106ef0a8ed2c4359a24d54970ab29
Elliot Akande <elbo.lynch@gmail.com>
2023-08-18 09:37:03 +0100

    Add icons to contact page

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,4 +1,6 @@
 import MapImg from './map.png';
+import PhoneIcon from './phone.svg';
+import MapIcon from './map-marker.svg';
 
 export default function contactPage() {
     const main = document.querySelector('main');
@@ -18,20 +20,29 @@ export default function contactPage() {
         card.classList.add('card');
         main.appendChild(card);
 
+        //  Phone 
         const phone = document.createElement('div');
         card.appendChild(phone);
+
+        const phoneIcon = document.createElementNS(PhoneIcon, 'svg');
+        phone.appendChild(phoneIcon);
 
         const paraOne = document.createElement('p');
         paraOne.textContent = '07572 893017';
         phone.appendChild(paraOne);
 
+        //  Address 
         const address = document.createElement('div');
         card.appendChild(address);
+
+        const mapIcon = document.createElementNS(MapIcon, 'svg');
+        phone.appendChild(mapIcon);
 
         const paraTwo = document.createElement('p');
         paraTwo.textContent = '123 Sesame Street, Ardrossan, Scotland';
         phone.appendChild(paraTwo);
 
+        //  Map
         const img = document.createElement('img');
         img.src = MapImg;
         card.appendChild(img);
